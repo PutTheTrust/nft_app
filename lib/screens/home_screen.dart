299f201b8268377e1ef24afee115cfdb40c93abb
Bekithemba <putthetrust.gamedev@gmail.com>
2023-04-27 19:49:06 +0200

    add: hot deals nfts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,83 +2,143 @@ import 'package:flutter/material.dart';
 import 'package:nft_app/widgets/input_field.dart';
 import 'package:nft_app/widgets/nft_card.dart';
 
+import '../widgets/hot_nft_card.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  static const List nfts = [
+    ["Doodles #7575", "04 : 02 : 08", "20.8", "assets/images/nft1.png"],
+    ["Doodles #862", "04 : 02 : 08", "15.5", "assets/images/nft2.png"],
+  ];
+
+  // ignore: constant_identifier_names
+  static const List hot_nfts = [
+    ["Doodles #862", "04 : 02 : 08", "10.2", "assets/images/nft3.png"],
+    ["Doodles #862", "04 : 02 : 08", "10.2", "assets/images/nft4.png"]
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-        child: Column(
-          children: [
-            Row(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+            child: Column(
               children: [
-                Image.asset("assets/images/user.png"),
-                const SizedBox(
-                  width: 8,
+                Row(
+                  children: [
+                    Image.asset("assets/images/user.png"),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Welcome,",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        Text(
+                          "Vitamin Moly ✨",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(12)),
+                      child: const Icon(
+                        Icons.notifications,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const SizedBox(
+                  height: 20,
+                ),
+                const InputField(),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: const [
                     Text(
-                      "Welcome,",
-                      style: TextStyle(color: Colors.grey),
+                      "Popular Now",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
                     ),
                     Text(
-                      "Vitamin Moly ✨",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                      "View all",
+                      style: TextStyle(fontSize: 16),
                     )
                   ],
                 ),
-                const Spacer(),
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Icon(
-                    Icons.notifications,
-                    color: Colors.green,
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 260,
+                  // color: Colors.green,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: nfts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return (NftCard(
+                        title: nfts[index][0],
+                        time: nfts[index][1],
+                        price: nfts[index][2],
+                        url: nfts[index][3],
+                      ));
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Hot Deals",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    ),
+                    Text(
+                      "View all",
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 260,
+                  // color: Colors.green,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: hot_nfts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return (HotNftCard(
+                        title: hot_nfts[index][0],
+                        time: hot_nfts[index][1],
+                        price: hot_nfts[index][2],
+                        url: hot_nfts[index][3],
+                      ));
+                    },
                   ),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            const InputField(),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text(
-                  "Popular",
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-                ),
-                Text(
-                  "View all",
-                  style: TextStyle(fontSize: 16),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 260,
-              // color: Colors.green,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  NftCard(),
-                  NftCard(),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/hot_nft_card.dart
+++ lib/widgets/hot_nft_card.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class NftCard extends StatelessWidget {
+class HotNftCard extends StatelessWidget {
   final String title;
   final String time;
   final String price;
   final String url;
 
-  const NftCard(
+  const HotNftCard(
       {super.key,
       required this.title,
       required this.time,
@@ -15,8 +15,8 @@ class NftCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 100,
-        width: 219,
+        height: 192,
+        width: 172,
         margin: const EdgeInsets.only(right: 16),
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -28,21 +28,31 @@ class NftCard extends StatelessWidget {
           children: [
             Container(
               padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(12),
+              margin: const EdgeInsets.only(bottom: 45, left: 10, right: 10),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(12),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text(title), Text(time)],
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      Text(
+                        time,
+                        style: TextStyle(fontSize: 8),
+                      )
+                    ],
                   ),
                   Container(
                     padding: const EdgeInsets.all(8),
+                    // width: 25,
                     decoration: BoxDecoration(
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(10),
@@ -51,7 +61,7 @@ class NftCard extends StatelessWidget {
                       Image.asset("assets/images/ethereum.png"),
                       const Text(
                         "15.5",
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, fontSize: 10),
                       )
                     ]),
                   )
